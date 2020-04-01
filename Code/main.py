edf2bac96c3f76cddc0e237417bf524c20649cee
greggbernelegarda <gregglegarda@gmail.com>
2020-04-01 16:58:21 -0400

    Created geo map with datapoints

--- Code/main.py
+++ Code/main.py
@@ -1,20 +1,23 @@
 
 import plotly.express as px
 import pandas as pd
-
-
 import plotly.graph_objects as go
-
 import pandas as pd
+
+
+#########-------------------------------------- DATA FRAME -------------------------------------- #########
 df = pd.read_csv("US_Accidents_Dec19.csv")
 #df = pd.read_csv("https://www.kaggle.com/sobhanmoosavi/us-accidents#US_Accidents_Dec19.csv")
-df = df.sample(10000)
+#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv')
+df = df.sample(100)
+print(df.columns)
+del df['Source']
+print(df.columns)
 print(df.head(10))
 
 
 
-#df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv')
-df['text'] = "helo"
+df['text'] = df['Street'] +', '+ df['City']+ ', '+ df['County']+ ' ' + df['State']+ ' '+ df['Zipcode'].astype(str)
 scl = [0,"rgb(150,0,90)"],[0.125,"rgb(0, 0, 200)"],[0.25,"rgb(0, 25, 255)"],\
 [0.375,"rgb(0, 152, 255)"],[0.5,"rgb(44, 255, 150)"],[0.625,"rgb(151, 255, 0)"],\
 [0.75,"rgb(255, 234, 0)"],[0.875,"rgb(255, 111, 0)"],[1,"rgb(255, 0, 0)"]
